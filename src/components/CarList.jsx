67c056e4885e998f05450fb90638b96cb4f1fa38
희호 <kim21670@naver.com>
2025-07-22 00:02:42 +0900

    useMemo vs React.memo 주석 추가

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
+// 기존 car 컴포넌트들의 props인 car 객체가 변경되지 않음을 감지하고 불필요한 리렌더링 방지
 const Car = React.memo(({ car }) => {
     console.log(`Rendering ${car.model}`);
     return (
@@ -32,5 +33,7 @@ const CarList = () => {
         </div>
     )
 }
+
+// useMemo는 값을 메모이제이션하여 연산을 최적화하고, React.memo는 컴포넌트 전체를 메모이제이션해 불필요한 렌더링을 방지
 
 export default CarList;
